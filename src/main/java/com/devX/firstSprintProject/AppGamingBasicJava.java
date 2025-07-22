464241ac1c322a350fc994a04a54366c85a9b7b8
Abhay Lonkar <abhaylonkar9@gmail.com>
2025-07-22 13:52:13 +0530

    added a PacManGame class

--- src/main/java/com/devX/firstSprintProject/AppGamingBasicJava.java
+++ src/main/java/com/devX/firstSprintProject/AppGamingBasicJava.java
@@ -2,6 +2,7 @@ package com.devX.firstSprintProject;
 
 import com.devX.firstSprintProject.game.GameRunner;
 import com.devX.firstSprintProject.game.MarioGame;
+import com.devX.firstSprintProject.game.PacManGame;
 import com.devX.firstSprintProject.game.SuperContraGame;
 
 public class AppGamingBasicJava {
@@ -10,7 +11,8 @@ public class AppGamingBasicJava {
 		// TODO Auto-generated method stub
 		
 //		var game = new MarioGame();
-		var game = new SuperContraGame();
+//		var game = new SuperContraGame();
+		var game = new PacManGame();
 		var gameRunner = new GameRunner(game);
 		gameRunner.run();
 	}
---
+++ src/main/java/com/devX/firstSprintProject/game/PacManGame.java
@@ -0,0 +1,30 @@
+package com.devX.firstSprintProject.game;
+
+public class PacManGame implements GamingConsole{
+
+	@Override
+	public void up() {
+		// TODO Auto-generated method stub
+		System.out.println("Move Up");
+		
+	}
+
+	@Override
+	public void down() {
+		// TODO Auto-generated method stub
+		System.out.println("Move Down");
+	}
+
+	@Override
+	public void right() {
+		// TODO Auto-generated method stub
+		System.out.println("Move Right");
+	}
+
+	@Override
+	public void left() {
+		// TODO Auto-generated method stub
+		System.out.println("Move Left");
+	}
+
+}
