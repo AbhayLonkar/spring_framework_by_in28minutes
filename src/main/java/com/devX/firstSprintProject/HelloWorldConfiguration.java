aa044016964111826089e51eb46e9a0051d43053
Abhay Lonkar <abhaylonkar9@gmail.com>
2025-07-23 11:11:27 +0530

    called beans method inside another bean using 1. Direct bean method calling and 2. Passing as Parameter

--- src/main/java/com/devX/firstSprintProject/HelloWorldConfiguration.java
+++ src/main/java/com/devX/firstSprintProject/HelloWorldConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 //Record is released in JDK 16
-record Person (String name, int age) {};
+record Person (String name, int age, Address address) {};
 record Address (String firstline, String city) {};
 
 
@@ -23,12 +23,25 @@ public class HelloWorldConfiguration {
 	
 	@Bean 
 	public Person person() {
-		return new Person("Ravi",21);
+		return new Person("Ravi",21, new Address("Mihan", "Nagpur"));
 	}
+	
+	@Bean
+	public Person person2MethodCall() {
+		// Calling Beans method directly
+		return new Person(name(), age(), address()); // name, age, address
+	}
+	
+	@Bean
+	public Person person3Parameters(String name, int age, Address getAddress) { // name, age, getAddress
+		// Passing as parameters
+		return new Person(name, age, getAddress); // name, age
+	}
+
 
 	@Bean(name = "getAddress")
 	public Address address() {
-		return new Address("Mihan", "Nagpur");
+		return new Address("Baker Street", "London");
 	}
 
 }
